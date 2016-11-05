fefce4d5ec8d9f285102ba72a3c1d38ed180e1e7
Adam7e7 <Adam.helmy@mail.mcgill.ca>
2016-11-05 17:33:57 -0400

    Update Mapping.java

--- Mapping.java
+++ Mapping.java
@@ -1,14 +1,29 @@
-package collector;
+package trotty02;
 
 public class Mapping {
-	int[][] map;
-	ColorPoller colPol;
+	int[][] map = new int[12][12];
+	//blocks are 1
+	//empty or unexplored space is 0
 	
-	public Mapping(int[][] map){
-		this.map = map;
-		this.colPol = colPol;
+	
+	public Mapping(){
+		for(int i = 0; i < 12; i++){
+			for(int j = 0; j < 12; j++){
+				map[i][j] = 0;
+			}
+		}
+		//sets up empty map
 	}
 	
+	public int readMapAt(int x, int y){
+		return map[x][y];
+	}
 	
+	public void updateMapAt(int x, int y, int T, int distance){
+		//takes in current bot position and usPoller distance
+		//updates the map in the square where it saw the block
+		map[(int)((x+distance*Math.cos(T))/30)][(int)((y+distance*Math.sin(T))/30)] = 1;
+		
+	}
 	
 }
