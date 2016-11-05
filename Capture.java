771e9400a160777fb40b63751bca357bc3566828
Adam7e7 <Adam.helmy@mail.mcgill.ca>
2016-11-05 17:03:10 -0400

    Update Capture.java

--- Capture.java
+++ Capture.java
@@ -1,5 +1,17 @@
-package collector;
+package trotty02;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class Capture {
+	public EV3LargeRegulatedMotor armMotor;
+
+	Capture(EV3LargeRegulatedMotor armMotor){
+		this.armMotor = armMotor;
+	}
+	public void CaptureObj(){
+		armMotor.rotateTo(50);
+		//TODO: determine actualy angle- 50 was a random number
+	}
 
 }
