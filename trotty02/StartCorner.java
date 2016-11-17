40b2f58148033137612fcf3bfe1e6b5a0b87eedc
Ké Zhang <kikiz24301@gmail.com>
2016-11-17 16:54:29 -0500

    API Docs

--- trotty02/StartCorner.java
+++ trotty02/StartCorner.java
@@ -15,34 +15,58 @@ public enum StartCorner {
 
 	private int id, x, y;
 	private String name;
-
+	/**
+	 * Constructor for StartCorner, which determines which corner of the board the robot starts at
+	 * @param id id argument represents the chosen corner
+	 * @param x the start value on the x axis
+	 * @param y the start value on the y axis
+	 * @param name argument that identifies which role the robot will have during the game
+	 */
 	private StartCorner(int id, int x, int y, String name) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
 		this.name = name;
 	}
-
+	/**
+	 * acquires the name in a readable string format
+	 */
 	public String toString() {
 		return this.name;
 	}
-
-	public int[] getCooridinates() {
+	/**
+	 * gets the coordinates
+	 * @return the x and the y values in an int array
+	 */
+	public int[] getCoordinates() {
 		return new int[] { this.x, this.y };
 	}
-
+	/**
+	 * gets the x value and returns it as an int 
+	 * @return the x value
+	 */
 	public int getX() {
 		return this.x;
 	}
-
+	/**
+	 * gets the y values and returns it as an int
+	 * @return
+	 */
 	public int getY() {
 		return this.y;
 	}
-
+	/**
+	 * gets the role identification information and returns it as an int
+	 * @return the id value received
+	 */
 	public int getId() {
 		return this.id;
 	}
-
+	/**
+	 * Ensures that the corner values correspond to the corner information received 
+	 * @param cornerId the corner information received
+	 * @return void
+	 */
 	public static StartCorner lookupCorner(int cornerId) {
 		for (StartCorner corner : StartCorner.values())
 			if (corner.id == cornerId)
